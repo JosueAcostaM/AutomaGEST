8028917dc55bf8be33a419bcb30f63d25b32c056
Ander <andersonsarabia200412@gmail.com>
2026-02-04 07:12:19 -0500

    Cambios en las vistas

--- MVC_AutomaG/Controllers/LoginController.cs
+++ MVC_AutomaG/Controllers/LoginController.cs
@@ -78,6 +78,7 @@ namespace MVC_AutomaG.Controllers
             // Elimina la cookie de autenticación
             await HttpContext.SignOutAsync("Cookies");
             return RedirectToAction("Index", "Home");
+           //return RedirectToAction("Index", "Login");
         }
 
         //[HttpGet]
